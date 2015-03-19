b748f3f6616f4dd174f5dbd7897e8e6c5c479297
Flavio Fernandes <ffernand@redhat.com>
2015-03-19 15:48:03 -0400

    Add zlib-devel, do not disable repos when enabling epel repo

--- provision.sh
+++ provision.sh
@@ -8,13 +8,13 @@ yum install -y epel-release
 sed -i -e 's/enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
 
 # install packages that are in epel (i.e. not part of centos standard repo)
-yum install -y --disablerepo="*" --enablerepo="epel" scons protobuf protobuf-devel protobuf-compiler
+yum install -y --enablerepo="epel" scons protobuf protobuf-devel protobuf-compiler
 
 # install centos packages
-yum install -y git python-lxml wget gcc patch make unzip flex bison gcc-c++ openssl-devel autoconf automake vim python-devel python-setuptools  
+yum install -y git python-lxml wget gcc patch make unzip flex bison gcc-c++ openssl-devel autoconf automake vim python-devel python-setuptools
 
 # install centos packages that were not needed in centos 6.x, but are needed on centos 7
-yum install -y libtool kernel-devel bzip2 boost-devel tbb-devel libcurl-devel libxml2-devel
+yum install -y libtool kernel-devel bzip2 boost-devel tbb-devel libcurl-devel libxml2-devel zlib-devel
 
 echo "***********************************************************"
 echo "*  PREPARE opencontrail repo and RELOAD THIS VAGRANT BOX  *"
